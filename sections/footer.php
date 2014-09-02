9991bd8a7f246f4b1282fe69e076738dda8a9b90
cchatterton <chris@techn.com.au>
2014-09-02 21:13:19 +1000

    tweaks

--- sections/footer.php
+++ sections/footer.php
@@ -1,1 +1,6 @@
-<div class="columns">footer</div>
+<div class="columns">
+cd <?php echo $_SERVER['DOCUMENT_ROOT']; ?>/wp-admin/<br>
+chown <?php echo(exec("whoami")); ?>: plugin-install.php theme-install.php update.php<br>
+cd	<?php echo $_SERVER['DOCUMENT_ROOT']; ?>/wp-content<br>
+chown <?php echo(exec("whoami")); ?>: -R plugins/*<br>
+</div>
